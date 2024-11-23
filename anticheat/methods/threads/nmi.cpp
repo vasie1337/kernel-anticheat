912fb153ff4d532b0f923b53eb16305220629bfb
Vasie <59412971+Vasie1337@users.noreply.github.com>
2024-11-23 13:24:18 +0100

    NMI rework

--- anticheat/methods/threads/nmi.cpp
+++ anticheat/methods/threads/nmi.cpp
@@ -1,134 +1,78 @@
 #include "nmi.h"
 
-void*               g_NmiCallbackHandle;
-PKAFFINITY_EX       g_NmiAffinity;
-PNMI_CONTEXT        g_NmiContext;
-void*               g_PageOfpStackWalkResult;
-BOOLEAN             NMIStop;
-HANDLE              SendNMIThreadHandle;
-
-BOOLEAN FireNMI(INT core, PKAFFINITY_EX affinity)
+void nmi::init()
 {
-	KeInitializeAffinityEx(affinity);
-	KeAddProcessorAffinityEx(affinity, core);
+	core_count = KeQueryActiveProcessorCountEx(0);
+	context_size = core_count * sizeof(NMI_CONTEXT);
 
-	HalSendNMI(affinity);
-
-	return TRUE;
-}
-
-VOID DetectionThread(void* StartContext)
-{
-	UNREFERENCED_PARAMETER(StartContext);
-
-	while (!NMIStop)
+	context = reinterpret_cast<PNMI_CONTEXT>(ExAllocatePool(NonPagedPool, context_size));
+	if (!context)
 	{
-		// Fire an NMI for eac core on the system with 100 ms delay
-		for (ULONG i = 0; i < KeQueryActiveProcessorCountEx(0); i++)
-		{
-			FireNMI(i, g_NmiAffinity);
-			DelayExecutionThread(100);
-		}
-
-		// Loop through the g_PageOfpStackWalkResult list
-		for (INT i = 0; i < 0x1000 / 0x10; i += 2)
-		{
-			// Check if current item of g_PageOfpStackWalkResult is there
-			if (((DWORD64*)g_PageOfpStackWalkResult)[i] == 0)
-				continue;
-
-			// Check if the stackTrace is valid and there are captured frames to loop through
-			if (MmIsAddressValid(((void**)g_PageOfpStackWalkResult)[i]) && ((DWORD64*)g_PageOfpStackWalkResult)[i + 1])
-			{
-				// Loop through the captured frames
-				for (SIZE_T j = 0; i < ((DWORD64*)g_PageOfpStackWalkResult)[i + 1]; j++)
-				{
-					ULONG64 CurrentFrameValue = (((DWORD64**)g_PageOfpStackWalkResult)[i])[j];
-
-					// Check if the CurrentFrameValue is an address in kernel 
-					if (CurrentFrameValue < 0xFFFF000000000000)
-						break;
-
-					// Check if CurrentFrameValue is in a kernel module.
-					// If not, we have found something.
-					if (IsAdressOutsideModulelist(CurrentFrameValue))
-						DbgPrintEx(0, 0, "Unsigned code : %llx", CurrentFrameValue);
-				}
-			}
-
-			// Remove the stackTrace and the capturedFrames out of the list,
-			// So the NMI callback can store new ones.
-			ExFreePoolWithTag(((void**)g_PageOfpStackWalkResult)[i], AC_POOL_TAG);
-			((DWORD64*)g_PageOfpStackWalkResult)[i] = 0;
-			((DWORD64*)g_PageOfpStackWalkResult)[i + 1] = 0;
-		}
-	}
-	PsTerminateSystemThread(STATUS_SUCCESS);
-}
-
-
-BOOLEAN NmiCallback(void* context, BOOLEAN handled)
-{
-	UNREFERENCED_PARAMETER(context);
-	UNREFERENCED_PARAMETER(handled);
-
-	void** stackTrace = ExAllocatePoolWithTag(NonPagedPool, 0x1000, AC_POOL_TAG);
-
-	if (!stackTrace)
-		return TRUE;
-
-	// Captures a stack back trace by walking up the stack and recording the information for each frame 
-	USHORT capturedFrames = RtlCaptureStackBackTrace(0, 0x1000 / 8, stackTrace, NULL);
-
-	// Loop through g_PageOfpStackWalkResult list, 
-	// if empty it has been checked by the DetectionThread,
-	// and we can store the stackTrace and the capturedFrames in the list.
-	for (int i = 0; i < 0x1000 / 0x10; i += 2)
-	{
-		if (((DWORD64*)g_PageOfpStackWalkResult)[i] == 0)
-		{
-			((DWORD64*)g_PageOfpStackWalkResult)[i] = (ULONG64)stackTrace;
-			((DWORD64*)g_PageOfpStackWalkResult)[i + 1] = capturedFrames;
-			break;
-		}
-	}
-
-	return TRUE;
-}
-
-VOID StartNMI()
-{
-	DbgPrintEx(0, 0, "Started NMI callback : (Could produce false positives)");
-
-	ULONG numCores = KeQueryActiveProcessorCountEx(0);
-	ULONG nmiContextLength = numCores * sizeof(NMI_CONTEXT);
-
-	g_NmiContext = (PNMI_CONTEXT)ExAllocatePoolWithTag(NonPagedPool, nmiContextLength, AC_POOL_TAG);
-	g_NmiAffinity = ExAllocatePoolWithTag(NonPagedPool, sizeof(KAFFINITY_EX), AC_POOL_TAG);
-	g_PageOfpStackWalkResult = ExAllocatePoolWithTag(NonPagedPool, 0x1000, AC_POOL_TAG);
-
-	g_NmiCallbackHandle = KeRegisterNmiCallback(NmiCallback, g_NmiContext);
-
-	if (!g_NmiAffinity || !g_NmiContext || !g_NmiCallbackHandle || !g_PageOfpStackWalkResult)
 		return;
+	}
 
-	memset(g_NmiContext, 0, nmiContextLength);
-	memset(g_PageOfpStackWalkResult, 0, 0x1000);
+	mask = reinterpret_cast<PKAFFINITY_EX>(ExAllocatePool(NonPagedPool, sizeof(KAFFINITY_EX)));
+	if (!mask)
+	{
+		ExFreePool(context);
+		return;
+	}
 
-	PsCreateSystemThread(&SendNMIThreadHandle, 0, NULL, NULL, NULL, &DetectionThread, NULL);
+	results = ExAllocatePool(NonPagedPool, PAGE_SIZE);
+	if (!results)
+	{
+		ExFreePool(mask);
+		ExFreePool(context);
+		return;
+	}
+
+	RtlZeroMemory(context, context_size);
+	RtlZeroMemory(mask, sizeof(KAFFINITY_EX));
+	RtlZeroMemory(results, PAGE_SIZE);
+
+	did_init = true;
 }
 
-VOID StopNMI()
+bool nmi::scan(int count)
 {
+	if (!did_init)
+	{
+		return false;
+	}
 
-	NMIStop = TRUE;
-	WaitThreadTerminate(SendNMIThreadHandle);
+	nmi_handle = KeRegisterNmiCallback(callback, nullptr);
+	if (!nmi_handle)
+	{
+		return false;
+	}
 
-	if (g_NmiCallbackHandle) KeDeregisterNmiCallback(g_NmiCallbackHandle);
-	if (g_NmiAffinity) ExFreePoolWithTag(g_NmiAffinity, AC_POOL_TAG);
-	if (g_NmiContext) ExFreePoolWithTag(g_NmiContext, AC_POOL_TAG);
-	if (g_PageOfpStackWalkResult) ExFreePoolWithTag(g_PageOfpStackWalkResult, AC_POOL_TAG);
+	LARGE_INTEGER delay = { 0 };
+	delay.QuadPart = -(10 * 100); // 10ms
 
-	DbgPrintEx(0, 0, "Stopped NMI callback");
+	for (int i = 0; i < count; i++)
+	{
+		for (int j = 0; j < core_count; j++)
+		{
+			fire_nmi(j, mask);
+			KeDelayExecutionThread(KernelMode, FALSE, &delay);
+		}
+	}
 
+	KeDeregisterNmiCallback(nmi_handle);
+
+	return true;
+}
+
+void nmi::fire_nmi(int core, PKAFFINITY_EX mask)
+{
+	KeInitializeAffinityEx(mask);
+	KeAddProcessorAffinityEx(mask, core);
+	HalSendNMI(mask);
+}
+
+BOOLEAN nmi::callback(PVOID context, BOOLEAN handled)
+{
+	DbgPrintEx(0, 0, "NMI fired\n");
+
+	return true;
 }
--- anticheat/methods/threads/nmi.h
+++ anticheat/methods/threads/nmi.h
@@ -4,7 +4,31 @@
 class nmi
 {
 public:
+	void init();
+
+	/// <summary>
+	/// Scan cores for suspicious activity by firing NMI
+	/// </summary>
+	/// <param name="count"> 
+	/// Number of times each core should be scanned
+	/// </param>
+	/// <returns>
+	/// True if successful, false otherwise
+	/// </returns>
+	bool scan(int count);
 
 private:
+	void fire_nmi(int core, PKAFFINITY_EX mask);
+	
+	static BOOLEAN callback(PVOID context, BOOLEAN handled);
 
+	ULONG core_count = 0;
+	ULONG context_size = 0;
+
+	PNMI_CONTEXT context = nullptr;
+	PKAFFINITY_EX mask = nullptr;
+	void* results = nullptr;
+	HANDLE nmi_handle = nullptr;
+
+	bool did_init = false;
 };
